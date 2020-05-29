3d8a76bdb7a9aeceddc4d88c32f3c7accb295360
arnabnandy <arnab.nandy1991@gmail.com>
2020-05-30 02:38:51 +0530

    Removed Unecessary lines

--- vars/kafkaInstall.groovy
+++ vars/kafkaInstall.groovy
@@ -2,28 +2,11 @@
 
 def ansibleDeploy(String deploymentType, String hostFile, String mainPlaybook) {
     if (deploymentType == "all") {
-        // def sout = new StringBuffer(), serr = new StringBuffer()
-        // def command = 'pwd && ls'
         def command = 'ansible-playbook -i ' + hostFile + ' ' + mainPlaybook
         sh command
-        // println command
-        // def proc = command.execute()
-        // proc.consumeProcessOutput(sout, serr)
-        // proc.waitFor()
-        // println "Std Out: ${proc.in.text}"
-        // println serr.tokenize()
-        // return sout.tokenize()
     }
 }
 
 def getCurrDir() {
-    // def sout = new StringBuffer(), serr = new StringBuffer()
-    // def cmd = "pwd"
-    // def proc = cmd.execute()
-    // proc.consumeProcessOutput(sout, serr)
-    // proc.waitFor()
-    // println "Std Out: ${proc.in.text}"
-    // println serr.tokenize()
-    // return sout.tokenize()
     sh 'pwd'
 }
