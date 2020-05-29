d4429b378e7c38c11afd85e4e51e67b7a27a0fa9
arnabnandy <arnab.nandy1991@gmail.com>
2020-05-30 03:41:04 +0530

    Changed library

--- vars/filterLogs.groovy
+++ vars/filterLogs.groovy
@@ -5,7 +5,7 @@ import org.apache.commons.lang.StringUtils
 def call(int occurence) {
     def logs = currentBuild.rawBuild.getLog(1000000).join('\n')
     int count = StringUtils.countMatches(logs, 'WARNING')
-    if (count > 3) {
+    if (count > occurence) {
         currentBuild.result = 'UNSTABLE'
     }
 }
